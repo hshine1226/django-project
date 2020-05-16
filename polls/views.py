d51e599aaae2baa457a47bb63fb5ea253b9866ba
hshine1226 <hshine1226@gmail.com>
2020-05-16 20:56:57 +0900

    views와 html 페이지에 render해서 보여줌

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,17 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("안녕하세요. 당신은 polls index에 있습니다.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
