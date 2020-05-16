f8a6ffdc92b5999ae1589c381733a648b47eda92
hshine1226 <hshine1226@gmail.com>
2020-05-16 20:38:37 +0900

    polls에 views.py를 추가함

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("안녕하세요. 당신은 polls index에 있습니다.")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}."
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
