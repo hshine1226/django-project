2c2327afca2ecfe42f105c7e790f2a394e2bbacf
hshine1226 <hshine1226@gmail.com>
2020-05-16 21:06:01 +0900

    django shortcuts의 get_object_or_404를 사용해서 Http404 예외를 발생시킬 수 있다.

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse
 
@@ -16,7 +17,9 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
